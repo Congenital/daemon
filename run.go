d0920398ebbc92d5524314bb5b2d16c0d7026c45
qiang.sheng@godinsec.com <qiang.sheng@godinsec.com>
2015-08-28 17:24:36 +0800

    change run program find, from the current path to Abssolutely path

--- run.go
+++ run.go
@@ -21,30 +21,40 @@ func main() {
 	filePath, err := filepath.Abs(os.Args[1])
 	if err != nil {
 		log.Error(err)
-		return
 	}
 
-	server := exec.Command(filePath, os.Args[2:]...)
-	server.Stdout = os.Stdout
-	server.Stderr = os.Stderr
-	server.Stdin = os.Stdin
-
-	var path string
-
-	if GetRelative(path) >= 0 {
-		path = GetPath(os.Args[1])
-		server.Dir = path
-	}
-
+	server := Exec(filePath, os.Args[2:]...)
 	err = server.Start()
 	if err != nil {
-		log.Error(err)
+		filePath = os.Args[1]
+		server = Exec(filePath, os.Args[2:]...)
+		err2 := server.Start()
+		if err2 != nil {
+			log.Error(err2)
+			return
+		}
 		return
 	}
 
 	log.Info("Start Daemon Server Ok!\n", os.Args[1])
 
 	return
+}
+
+func Exec(path string, args ...string) *exec.Cmd {
+	server := exec.Command(path, args...)
+	server.Stdout = os.Stdout
+	server.Stderr = os.Stderr
+	server.Stdin = os.Stdin
+
+	var _path string
+
+	if GetRelative(path) >= 0 {
+		path = GetPath(os.Args[1])
+		server.Dir = _path
+	}
+
+	return server
 }
 
 func GetPath(path string) string {
